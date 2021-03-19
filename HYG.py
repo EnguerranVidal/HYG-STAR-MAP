ee12d663abadec88fee99d46498eef95aa3349a5
KeplerDream <engue11@hotmail.com>
2021-03-19 23:32:11 +0100

    Adding main files
    Adding the main .py files as well as the HYG data in 3 forms : .csv, .txt and .rar

---
+++ HYG.py
@@ -0,0 +1,145 @@
+# PROJECT DECEMBRE 2019
+# PROJECT STAR MAP / HYG
+# By Enguerran VIDAL
+
+# This file contains the main class of this project.
+
+###############################################################
+#                           IMPORTS                           #
+###############################################################
+
+#-----------------MODULES
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
+#-----------------PYTHON FILES
+from database import*
+
+###############################################################
+#                         HYG CLASS                           #
+###############################################################
+
+class HYG_Map():
+    ''' StarMap created from the HYG stellar database.'''
+    def __init__(self):
+        try:
+            txt_file='hygdatav3.txt'
+            print(" Loading HYG Database ... ")
+            labels,data=import_database(txt_file)
+            print(" Database loaded. ")
+            print(labels)
+            self.data=data
+            self.labels=labels
+            self.n_variables=len(labels)
+            self.n_stars=len(data)
+        except FileNotFoundError:
+            print("'"+txt_file+"' cannot be found in the main directory !")
+            raise
+
+    def find_label(self,label):
+        ''' Finds a possible label in the different columns of the HYG database'''
+        try:
+            for i in range(self.n_variables):
+                if label==self.labels[i]:
+                    return i
+            raise NameError("The '"+str(label)+"' column cannot be found in the HYG database.")
+        except NameError:
+            raise
+
+    def Hertzprung_Russell(self,n):
+        ''' Plots n stars in the standard Hertzsprung-Russell.
+            n --> number of stars'''
+        am=self.find_label('absmag')
+        ci=self.find_label('ci')
+        abs_mag=[]
+        color_index=[]
+        for i in range(self.n_stars):
+            if self.data[i][am]!='N' and self.data[i][ci]!='N':
+                abs_mag.append(float(self.data[i][am]))
+                color_index.append(float(self.data[i][ci]))
+        m=len(color_index)
+        multiple=int(m/n)+1
+        cis=[]
+        mags=[]
+        for i in range(m):
+            if i%multiple==0:
+                cis.append(color_index[i])
+                mags.append(abs_mag[i])
+        color_index=np.array(cis)
+        abs_mag=np.array(mags)
+        print(color_index.shape)
+        print(abs_mag.shape)
+        fig = go.Figure(data=go.Scatter(x=color_index,y=abs_mag,
+                                        mode='markers',
+                                        marker=dict(color=color_index,
+                                                    colorscale=star_colorscale(),
+                                                    line_width=1,
+                                                    cmax=2.0,cmin=-0.4)
+                                        ,))
+        fig.update_layout(title='Hertzsprung-Russell Diagram')
+        fig['layout']['yaxis']['autorange'] = "reversed"
+        fig.write_html('first_figure.html', auto_open=True)
+
+    def space_bubble(self,distance):
+        ''' Plots the stars around the Sun contained in a "bubble" of a given radius.
+            distance --> radius of the bubble'''
+        am=self.find_label('absmag')
+        ci=self.find_label('ci')
+        x=self.find_label('x')
+        y=self.find_label('y')
+        z=self.find_label('z')
+        d=self.find_label('dist')
+        X=[]
+        Y=[]
+        Z=[]
+        color_index=[]
+        for i in range(self.n_stars):
+            if self.data[i][x]!='N' and self.data[i][y]!='N' and self.data[i][z]!='N' and self.data[i][ci]!='N':
+                if float(self.data[i][d])*3.26156<distance:
+                    X.append(float(self.data[i][x])*3.26156)
+                    Y.append(float(self.data[i][y])*3.26156)
+                    Z.append(float(self.data[i][z])*3.26156)
+                    color_index.append(float(self.data[i][ci]))
+        fig = go.Figure(data=go.Scatter3d(x=X,y=Y,z=Z
+                                          ,mode='markers',
+                                          marker=dict(color=color_index,
+                                                      colorscale=star_colorscale(),
+                                                      line_width=1,
+                                                      cmax=2.0,cmin=-0.4)
+                                        ,))
+        fig.write_html('first_figure.html', auto_open=True)
+                
+
+###############################################################
+#                        FUNCTIONS                            #
+###############################################################
+
+def star_colorscale():
+    ''' Returns a Colorscale for stars color from their color indexes.'''
+    C=[[0.0,'rgb(155,178,255)'],[0.0208333333333333,'rgb(158,181,255)'],[0.0416666666666666,'rgb(163,184,255)'],
+       [0.0624999999999999,'rgb(170,191,255)'],[0.0833333333333332,'rgb(178,197,255)'],[0.1041666666666665,'rgb(187,204,255)'],
+       [0.1249999999999998,'rgb(196,210,255)'],[0.1458333333333331,'rgb(204,216,255)'],[0.1666666666666664,'rgb(211,221,255)'],
+       [0.1874999999999997,'rgb(218,226,255)'],[0.208333333333333,'rgb(223,229,255)'],[0.2291666666666663,'rgb(228,266,255)'],
+       [0.2499999999999996,'rgb(233,236,255)'],[0.2708333333333329,'rgb(238,239,255)'],[0.2916666666666662,'rgb(243,242,255)'],
+       [0.3124999999999995,'rgb(248,246,255)'],[0.3333333333333328,'rgb(254,249,255)'],[0.3541666666666661,'rgb(255,249,251)'],
+       [0.3749999999999994,'rgb(255,247,245)'],[0.3958333333333327,'rgb(255,245,239)'],[0.416666666666666,'rgb(255,243,234)'],
+       [0.4374999999999993,'rgb(255,241,229)'],[0.4583333333333326,'rgb(255,239,224)'],[0.4791666666666659,'rgb(255,237,219)'],
+       [0.4999999999999992,'rgb(255,235,214)'],[0.5208333333333325,'rgb(255,233,210)'],[0.5416666666666658,'rgb(255,232,206)'],
+       [0.5624999999999991,'rgb(255,230,202)'],[0.5833333333333324,'rgb(255,229,198)'],[0.6041666666666657,'rgb(255,227,195)'],
+       [0.624999999999999,'rgb(255,226,191)'],[0.6458333333333323,'rgb(255,224,187)'],[0.6666666666666656,'rgb(255,223,184)'],
+       [0.6874999999999989,'rgb(255,221,180)'],[0.7083333333333322,'rgb(255,219,176)'],[0.7291666666666655,'rgb(255,218,173)'],
+       [0.7499999999999988,'rgb(255,216,169)'],[0.7708333333333321,'rgb(255,214,165)'],[0.7916666666666654,'rgb(255,213,161)'],
+       [0.8124999999999987,'rgb(255,210,156)'],[0.833333333333332,'rgb(255,208,150)'],[0.8541666666666653,'rgb(255,205,143)'],
+       [0.8749999999999986,'rgb(255,200,133)'],[0.8958333333333319,'rgb(255,193,120)'],[0.9166666666666652,'rgb(255,183,101)'],
+       [0.9374999999999985,'rgb(255,168,75)'],[0.9583333333333318,'rgb(255,149,35)'],[0.9791666666666651,'rgb(255,123,0)'],
+       [1.0,'rgb(255,82,0)']]
+    return C
+
+
+###############################################################
+#                       MAIN PROGRAM                          #
+###############################################################
+
+x=HYG_Map()
+x.Hertzprung_Russell(20000)
---
+++ database.py
@@ -0,0 +1,55 @@
+# PROJECT DECEMBRE 2019
+# PROJECT STAR MAP / DATABASE
+# By Enguerran VIDAL
+
+# This file contains the database handling functions.
+
+###############################################################
+#                           IMPORTS                           #
+###############################################################
+
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+###############################################################
+#                        FUNCTIONS                            #
+###############################################################
+
+def csv2txt(csv_file,txt_file):
+    ''' Transforms a csv file into a txt file '''
+    with open(txt_file, "w") as my_output_file:
+        print(" Opened new txt file")
+        with open(csv_file, "r") as my_input_file:
+            print(" Opened old csv file")
+            [ my_output_file.write(",".join(row)+'\n') for row in csv.reader(my_input_file)]
+        my_output_file.close()
+
+def format_txt(txt_file,char1,char2):
+    ''' Changes "char1" into "char2" throughout an entire .txt file.'''
+    with open(txt_file,"r") as file:
+        lines=file.readlines()
+        file.close()
+    with open(txt_file,"w") as file:
+        n=len(lines)
+        for i in range(n):
+            lines[i]=lines[i].replace(char1,char2)
+            file.write(lines[i])
+        file.close()
+
+def import_database(txt_file):
+    ''' Returns the data from a .txt file transformed from a csv file'''
+    with open(txt_file,"r") as file:
+        lines=file.readlines()
+        n=len(lines)
+        for i in range(n):
+            lines[i]=lines[i].split(',')
+            m=len(lines[i])
+            for j in range(m):
+                if lines[i][j]=='' or lines[i][j]=='\n':
+                    lines[i][j]='N'
+    labels=lines[0]
+    lines.pop(0)
+    return labels,lines
+        
+
