edc3c40d348d076c61ffb9a46955f87b6e6cbf1b
KeplerDream <engue11@hotmail.com>
2021-06-29 02:58:24 +0200

    PEP Format and requirements
    Adding requirements.txt and formatting the code in accordance to the PEP format.

--- database.py
+++ database.py
@@ -9,50 +9,52 @@
 ###############################################################
 
 import csv
-import numpy as np
-import matplotlib.pyplot as plt
+
 
 ###############################################################
 #                        FUNCTIONS                            #
 ###############################################################
 
-def csv2txt(csv_file,txt_file):
+def csv2txt(csv_file, txt_file):
     ''' Transforms a csv file into a txt file '''
     with open(txt_file, "w") as my_output_file:
         print(" Opened new txt file")
         with open(csv_file, "r") as my_input_file:
             print(" Opened old csv file")
-            [ my_output_file.write(",".join(row)+'\n') for row in csv.reader(my_input_file)]
+            [my_output_file.write(",".join(row) + '\n') for row in csv.reader(my_input_file)]
         my_output_file.close()
 
-def format_txt(txt_file,char1,char2):
+
+def format_txt(txt_file, char1, char2):
     ''' Changes "char1" into "char2" throughout an entire .txt file.'''
-    with open(txt_file,"r") as file:
-        lines=file.readlines()
+    with open(txt_file, "r") as file:
+        lines = file.readlines()
         file.close()
-    with open(txt_file,"w") as file:
-        n=len(lines)
+    with open(txt_file, "w") as file:
+        n = len(lines)
         for i in range(n):
-            lines[i]=lines[i].replace(char1,char2)
+            lines[i] = lines[i].replace(char1, char2)
             file.write(lines[i])
         file.close()
 
+
 def import_database(txt_file):
     ''' Returns the data from a .txt file transformed from a csv file'''
-    with open(txt_file,"r") as file:
-        lines=file.readlines()
-        n=len(lines)
+    with open(txt_file, "r") as file:
+        lines = file.readlines()
+        n = len(lines)
         for i in range(n):
-            lines[i]=lines[i].split(',')
-            m=len(lines[i])
+            lines[i] = lines[i].split(',')
+            m = len(lines[i])
             for j in range(m):
-                if lines[i][j]=='' or lines[i][j]=='\n':
-                    lines[i][j]='N'
-    labels=lines[0]
+                if lines[i][j] == '' or lines[i][j] == '\n':
+                    lines[i][j] = 'N'
+    labels = lines[0]
     lines.pop(0)
-    return labels,lines
+    return labels, lines
 
-def dat2csv(dat_file,csv_file):
+
+def dat2csv(dat_file, csv_file):
     with open(dat_file) as infile, open(csv_file, "w") as outfile:
         csv_writer = csv.writer(outfile)
         prev = ''
@@ -60,4 +62,3 @@ def dat2csv(dat_file,csv_file):
         for line in infile.read().splitlines():
             csv_writer.writerow([line, prev])
             prev = line
-
